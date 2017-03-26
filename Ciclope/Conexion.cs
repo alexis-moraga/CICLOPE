d0f062b3bec091d8bcc08fec106f612d85176d40
alexis-moraga <alexis.a.moraga@gmail.com>
2017-03-26 02:51:37 -0300

    create class conecction

--- Ciclope/Conexion.cs
+++ Ciclope/Conexion.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+using MySql.Data.MySqlClient;
 namespace Ciclope
 {
     class Conexion
     {
+        MySqlConnecion Con = new MySqlConnecion("server=localhost; database=ciclope; uid=root; pwd=");
+    public void Cone(){
+
+            try
+            {
+                Con.Open();
+                MessageBox.Show("Conexion Realizada con exito");
+            }
+            catch (MySqlExeption ex)
+            {
+                MessageBox.Show("Error de Conexion");
+            }
+            finally
+            {
+                Con.Close();
+            }
+        }
     }
 }
